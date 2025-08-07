a4f635f65597efd110b0f41efffedc103fe7f48f
arthuryang2003 <m18621118532@163.com>
2025-08-07 21:24:30 +0800

    changes

--- domainbed/model_selection.py
+++ domainbed/model_selection.py
@@ -167,3 +167,87 @@ class LeaveOneOutSelectionMethod(SelectionMethod):
             return step_accs.argmax('val_acc')
         else:
             return None
+
+
+class OODValidationSelectionMethod(SelectionMethod):
+    name = "OOD validation set"
+
+    @classmethod
+    def _step_acc(self, record):
+        """Given a single record, return a {val_acc, test_acc} dict."""
+        test_env = record['args']['test_envs'][0]
+        val_envs = record['args']['val_envs']
+        val_env_keys = []
+        for i in itertools.count():
+            if f'env{i}_out_acc' not in record and f'env{i}_in_acc' not in record:
+                break
+            if i in val_envs:
+                val_env_keys.append(f'env{i}_out_acc')
+        test_in_acc_key = 'env{}_in_acc'.format(test_env)
+        return {
+            'val_acc': np.mean([record[key] for key in val_env_keys]),
+            'test_acc': record[test_in_acc_key]
+        }
+
+    @classmethod
+    def run_acc(self, run_records):
+        test_records = get_test_records(run_records)
+        if not len(test_records):
+            return None
+        return test_records.map(self._step_acc).argmax('val_acc')
+
+
+class IIDTrainingAccuracyMethod(SelectionMethod):
+    """Picks argmax(mean(env_out_acc for env in train_envs))"""
+    name = "Training accuracy"
+
+    @classmethod
+    def _step_acc(self, record):
+        """Given a single record, return a {val_acc, test_acc} dict."""
+        test_env = record['args']['test_envs'][0]
+        train_env_keys = []
+        val_env_keys = []
+        for i in itertools.count():
+            if f'env{i}_out_acc' not in record and f'env{i}_in_acc' not in record:
+                break
+            if i != test_env:
+                train_env_keys.append(f'env{i}_in_acc')
+                val_env_keys.append(f'env{i}_out_acc')
+        return {
+            'val_acc': np.mean([record[key] for key in val_env_keys]),
+            'test_acc': np.mean([record[key] for key in train_env_keys])
+        }
+
+    @classmethod
+    def run_acc(self, run_records):
+        test_records = get_test_records(run_records)
+        if not len(test_records):
+            return None
+        return test_records.map(self._step_acc).argmax('val_acc')
+
+
+class IIDValidationAccuracyMethod(SelectionMethod):
+    """Picks argmax(mean(env_out_acc for env in train_envs))"""
+    name = "IID validation accuracy"
+
+    @classmethod
+    def _step_acc(self, record):
+        """Given a single record, return a {val_acc, test_acc} dict."""
+        test_env = record['args']['test_envs'][0]
+        val_env_keys = []
+        for i in itertools.count():
+            if f'env{i}_out_acc' not in record and f'env{i}_in_acc' not in record:
+                break
+            if i != test_env:
+                val_env_keys.append(f'env{i}_out_acc')
+        return {
+            'val_acc': np.mean([record[key] for key in val_env_keys]),
+            'test_acc': np.mean([record[key] for key in val_env_keys])
+        }
+
+    @classmethod
+    def run_acc(self, run_records):
+        test_records = get_test_records(run_records)
+        if not len(test_records):
+            return None
+        return test_records.map(self._step_acc).argmax('val_acc')
