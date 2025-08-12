76132c2a614eb7d6953be40e02b48b585b73b326
arthuryang2003 <m18621118532@163.com>
2025-08-12 14:36:53 +0800

    visualize_cam change

--- domainbed/scripts/train.py
+++ domainbed/scripts/train.py
@@ -291,6 +291,8 @@ if __name__ == "__main__":
             evals = zip(eval_loader_names, eval_loaders, eval_weights)
 
             for name, loader, weights in evals:
+                if step != 0 :
+                    misc.check_single_class_prediction(algorithm, loader, device)
                 acc = misc.accuracy(algorithm, loader, weights, device)
                 results[name + '_acc'] = acc
 
