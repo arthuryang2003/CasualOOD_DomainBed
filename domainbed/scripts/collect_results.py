cd4ba4784b043212212aed6897982179e171b41c
arthuryang2003 <m18621118532@163.com>
2025-10-10 14:57:36 +0800

    change

--- domainbed/scripts/collect_results.py
+++ domainbed/scripts/collect_results.py
@@ -279,9 +279,9 @@ if __name__ == "__main__":
             # model_selection.IIDTrainingAccuracyMethod,
             # model_selection.IIDValidationAccuracyMethod
         ]
-        if args.algorithm is None or args.algorithm == "VITA":
-            SELECTION_METHODS.append(model_selection.VITAPreFTSelectionMethod)
-            SELECTION_METHODS.append(model_selection.VITA_Zu_only_SelectionMethod)
+    if args.algorithm == "VITA":
+        SELECTION_METHODS.append(model_selection.VITAPreFTSelectionMethod)
+        SELECTION_METHODS.append(model_selection.VITA_Zu_only_SelectionMethod)
 
     for selection_method in SELECTION_METHODS:
         if args.latex:
