cb89983d7d980d4e4022a66415c28672f8887a23
arthuryang2003 <m18621118532@163.com>
2025-10-13 14:11:14 +0800

    change

---
+++ sweep/COCOPlaces/run.sh
@@ -0,0 +1,18 @@
+dataset=COCOPlaces
+command=$1
+data_dir=$2
+gpu_id=$3
+
+CUDA_VISIBLE_DEVICES=${gpu_id} \
+python3 -m domainbed.scripts.sweep ${command}\
+       --datasets ${dataset}\
+       --algorithms VITA \
+       --data_dir ${data_dir}\
+       --command_launcher smart_gpu\
+       --fixed_test_envs 2\
+       --n_hparams 10\
+       --n_trials 3\
+       --skip_confirmation\
+       --hparams "$(<sweep/${dataset}/hparams.json)"\
+       --output_dir "sweep/${dataset}/VITA_IRM_Weight"
+
---
+++ sweep/COCOPlaces/run_baseline.sh
@@ -0,0 +1,17 @@
+dataset=COCOPlaces
+command=$1
+data_dir=$2
+gpu_id=$3
+
+CUDA_VISIBLE_DEVICES=${gpu_id} \
+python3 -m domainbed.scripts.sweep ${command}\
+       --datasets ${dataset}\
+       --algorithms Mixup CORAL DANN MTL SagNet RSC ANDMask RIDG LFME ASGDRO \
+       --data_dir ${data_dir}\
+       --command_launcher smart_gpu\
+       --fixed_test_envs 2\
+       --n_hparams 10\
+       --n_trials 3\
+       --skip_confirmation\
+       --hparams "$(<sweep/${dataset}/hparams.json)"\
+       --output_dir "sweep/${dataset}/baselines"
